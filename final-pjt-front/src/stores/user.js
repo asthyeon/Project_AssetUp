e0d741df3f3c6b135c76a7cbf78d51e8021b8236
gusrl123456 <gusrl123456@gmail.com>
2023-11-16 14:09:00 +0900

    feat/user

---
+++ final-pjt-front/src/stores/user.js
@@ -0,0 +1,60 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
+export const useUserStore = defineStore('user', () => {
+  const API_URL = 'http://127.0.0.1:8000'
+  const token = ref(null)
+  const router = useRouter()
+
+  // 회원가입 기능
+  const signUp = function (payload) {
+    const username = payload.username
+    const password1 = payload.password1
+    const password2 = payload.password2
+
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/signup/`,
+      data: {
+        username, password1, password2
+      }
+    })
+      .then(res => {
+        const password = password1
+        // 회원가입 하면 로그인 상태 유지하기
+        logIn({ username, password })
+      })
+      .catch(err => console.log(err))
+  }
+
+  // 로그인 기능
+  const logIn = function (payload) {
+    const username = payload.username
+    const password = payload.password
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/login/`,
+      data: {
+        username, password
+      }
+    })
+      .then(res => {
+        token.value = res.data.key
+        // 로그인 후 메인페이지로 이동
+        router.push({ name: 'main' })
+      })
+      .catch(err => console.log(err))
+  }
+
+  const isLogin = computed(() => {
+    if (token.value === null) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+  return { signUp, logIn, isLogin, token }
+}, { persist: true })
