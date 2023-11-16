434f6669f89cfe50010a6b2f6c3d5f71e15a5880
gusrl123456 <gusrl123456@gmail.com>
2023-11-16 12:50:43 +0900

    create project

---
+++ final-pjt-front/final-pjt-front/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{name:'main'}">Home</RouterLink> |
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import MainView from '@/views/MainView.vue'
+</script>
+
+<style scoped>
+
+</style>
---
+++ final-pjt-front/final-pjt-front/src/views/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>메인 페이지</h1>
+    <h2>금융 상품 비교 애플리케이션</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
